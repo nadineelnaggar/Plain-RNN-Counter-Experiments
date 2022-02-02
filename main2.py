0990f5e8ffbd866c6adde1991b228b3bdcccb896
nadineelnaggar <Nadine_el_naggar@hotmail.com>
2022-02-02 21:44:07 +0200

    remove the option to load a model

--- main2.py
+++ main2.py
@@ -79,7 +79,7 @@ print('num_layers = ',num_layers)
 print('learning_rate = ',learning_rate)
 print('num_epochs = ',num_epochs)
 print('num_runs = ',num_runs)
-print('load_model = ',load_model)
+# print('load_model = ',load_model)
 
 file_name = 'Dyck1_' + task + '_' + str(
         num_bracket_pairs) + '_bracket_pairs_' + model_name + '_Feedback_' + feedback + '_' + str(
@@ -313,12 +313,12 @@ def main():
 
         runs.append('run'+str(i))
         print('****************************************************************************\n')
-        train_accuracy, df = train(model, X_train, y_train)
+        train_accuracy, df = train(model, X_train[:200], y_train[:200])
         train_accuracies.append(train_accuracy)
         train_dataframes.append(df)
-        test_accuracy = test_model(model, X_test, y_test)
+        test_accuracy = test_model(model, X_test[:100], y_test[:100])
         test_accuracies.append(test_accuracy)
-        long_test_accuracy = test_model(model, X_long, y_long)
+        long_test_accuracy = test_model(model, X_long[:100], y_long[:100])
         long_test_accuracies.append(long_test_accuracy)
 
         with open(file_name, "a") as f:
