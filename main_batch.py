0e010146e5108afde2eecf3d2de6bec5c76b6c3d
nadineelnaggar <Nadine_el_naggar@hotmail.com>
2022-03-24 21:52:17 +0200

    checking if the if condition is correct when counting correct guesses

--- main_batch.py
+++ main_batch.py
@@ -467,8 +467,8 @@ def train(model, loader, sum_writer):
                 # print('target_np[j].shape = ',target_np[j].shape)
 
 
-                if np.equal(out_np[j][:length[j]].all(), target_np[j][:length[j]].all()):
-                # if out_np[j].all()==target_np[j].all():
+                # if np.equal(out_np[j][:length[j]].all(), target_np[j][:length[j]].all()):
+                if out_np[j].all()==target_np[j].all():
                 #     print('output_np[j] = target_np[j]')
                 #     count+=1
                 #     print('count correct = ',count)
