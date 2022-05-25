e22a07c468c80d9f5788bd726e68bc64052ca87d
nadineelnaggar <Nadine_el_naggar@hotmail.com>
2022-05-26 00:57:14 +0100

    code for creating the histograms

---
+++ create_inference_histograms.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn as nn
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+# to read the excel generated from the inference code
+# read the excel from the inference
+# make the histograms to reflect the distribution of the fpf (single model different sequences + max depth plot)
+# make the histograms to reflect the distribution of the fpf (single sequence different models + timestep plot)
