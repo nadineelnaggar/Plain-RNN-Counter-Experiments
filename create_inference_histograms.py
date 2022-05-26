0aa013a7493aa45b5ea4d1997981cdd823cd8360
nadineelnaggar <Nadine_el_naggar@hotmail.com>
2022-05-26 12:14:41 +0100

    change creation of histogram

--- create_inference_histograms.py
+++ create_inference_histograms.py
@@ -94,7 +94,7 @@ def create_histogram():
     for i in range(5):
         plt.subplots()
         bins = []
-        fpf = avg_fpf[i]
+        fpf = avg_fpf
         max_depths = max_depth[i]
         for k in range(len(fpf)):
             bins.append(k)
