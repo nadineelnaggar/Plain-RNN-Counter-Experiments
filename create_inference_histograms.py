dd2dc8a6759b783269154ad7ba5573043e0cc402
nadineelnaggar <Nadine_el_naggar@hotmail.com>
2022-05-31 23:30:50 +0100

    change fpf to assume that not all sequences were incorrectly predicted

--- create_inference_histograms.py
+++ create_inference_histograms.py
@@ -204,6 +204,7 @@ def create_histogram_one_sequence_multiple_models():
         for j in range(num_models):
             txt = df['first point of failure for each incorrect sequence'][j]
             all_fpf = [int(s) for s in txt.split(', ') if s.isdigit()]
+            # fpfs.append(all_fpf[i])
             for k in range(len(all_fpf)):
                 fpfs.append(all_fpf[k])
         plt.subplots()
