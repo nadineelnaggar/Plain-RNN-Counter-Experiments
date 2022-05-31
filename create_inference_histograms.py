735c2a7faaef0e65c798f0c8a377edbf982b434a
nadineelnaggar <Nadine_el_naggar@hotmail.com>
2022-05-31 22:52:43 +0100

    change range to include 10 sequences

--- create_inference_histograms.py
+++ create_inference_histograms.py
@@ -195,7 +195,7 @@ def create_histogram_one_sequence_multiple_models():
 
 
 
-    for i in range(10):
+    for i in range(len(dataset)):
         fpfs = []
         seq, label, length = dataset[i]
         timestep_depths = []
