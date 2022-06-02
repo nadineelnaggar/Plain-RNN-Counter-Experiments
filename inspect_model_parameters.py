9d4c1a82f407d4af2f2b2b62fa82f268b2572c6b
nadineelnaggar <Nadine_el_naggar@hotmail.com>
2022-06-02 14:26:24 +0100

    create code to display model parameters

--- inspect_model_parameters.py
+++ inspect_model_parameters.py
@@ -115,10 +115,11 @@ def inspect_model_parameters():
         model.to(device)
 
         if model_name=='VanillaReLURNN':
-            print('RNN weight_ih = ',model.rnn.weight_ih_l)
-            print('RNN weight_hh = ',model.rnn.weight_hh_l)
-            print('RNN bias_ih = ',model.rnn.bias_ih_l)
-            print('RNN bias_hh = ',model.rnn.bias_hh_l)
+            print(model.rnn.Parameters())
+            # print('RNN weight_ih = ',model.rnn.weight_ih_l)
+            # print('RNN weight_hh = ',model.rnn.weight_hh_l)
+            # print('RNN bias_ih = ',model.rnn.bias_ih_l)
+            # print('RNN bias_hh = ',model.rnn.bias_hh_l)
 
 
 inspect_model_parameters()
