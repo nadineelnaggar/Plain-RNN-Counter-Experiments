e9c4831ab12802e25ee57add6394171cdf0bb868
nadineelnaggar <Nadine_el_naggar@hotmail.com>
2022-06-16 14:41:41 +0100

    put tensors onto cpu to write to excel sheet

--- inspect_model_parameters.py
+++ inspect_model_parameters.py
@@ -299,7 +299,7 @@ def inspect_rnn(model):
     metric_hidden_weight = weights_hh.item()
 
 
-    return weights_ih[0][0].item().item().cpu(), weights_ih[0][1].item().item().cpu(), biases_ih.item().item().cpu(), weights_hh.item().item().cpu(), biases_hh.item().item().cpu(), metric_inc_dec_ratio.item().cpu(), metric_hidden_weight.item().cpu()
+    return weights_ih[0][0].item(), weights_ih[0][1].item(), biases_ih.item(), weights_hh.item(), biases_hh.item(), metric_inc_dec_ratio.item(), metric_hidden_weight.item()
 
 
 
