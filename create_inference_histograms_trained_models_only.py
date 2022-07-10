3cd9cbcc683c96df30ca627a0bd342399673a722
nadineelnaggar <Nadine_el_naggar@hotmail.com>
2022-07-10 14:50:28 +0100

    fix the selection of relevant rows

--- create_inference_histograms_trained_models_only.py
+++ create_inference_histograms_trained_models_only.py
@@ -115,6 +115,7 @@ def create_histogram():
     # df = read_sheets()
 
     if model_name == 'VanillaLSTM':
+        df['avg training losses'].dtype
         print(df.loc(df['avg training losses'] <= 10e-10).head())
         df = df.loc(df['avg training losses'] <= 10e-10)
         print(df.head())
