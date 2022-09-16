9c0e9ede2be0333130109d9f18e104cb471247cc
nadineelnaggar <Nadine_el_naggar@hotmail.com>
2022-09-16 17:28:43 +0100

    make the depth plot a little less opaque

--- create_inference_histograms_trained_models_only.py
+++ create_inference_histograms_trained_models_only.py
@@ -278,7 +278,7 @@ def create_histogram_one_sequence_multiple_models():
         # bins = [0,48, 49, 98, 99, 198, 199, 200, 248, 249, ]
         # bins = []
 
-        plt.plot([i for i in range(len(timestep_depth))], timestep_depth, color='red')
+        plt.plot([i for i in range(len(timestep_depth))], timestep_depth, color='red',alpha=0.5)
         plt.hist(fpfs, bins=new_bins)
         # plt.hist(fpfs, bins=range(0, 2001, 50))
         # plt.plot([i for i in range(len(timestep_depth))], timestep_depth, color='red')
