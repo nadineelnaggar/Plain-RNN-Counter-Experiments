f587f5b596abda7a98df8c9ed5a7296f1229b99e
nadineelnaggar <Nadine_el_naggar@hotmail.com>
2023-01-25 18:01:34 +0000

    code to extract relu model parameters, calculate indicators and perform linear regression analysis and write the results of linear regression to a text file

---
+++ create_regression_dataset.py
@@ -0,0 +1,22 @@
+x = []
+y = []
+lengths = []
+
+
+with open('Dyck1_Dataset_Suzgun_train_.txt', 'r') as f:
+    for line in f:
+        line = line.split(",")
+        sentence = line[0].strip()
+        label = line[1].strip()
+        x.append(sentence)
+        # y.append(label)
+        lengths.append(len(x))
+
+
+
+def getTimestepDepths():
+    for j in range(len(x)):
+        elem = x[j]
+        depth = 0
+        for i in range(len(elem)):
+            pass
