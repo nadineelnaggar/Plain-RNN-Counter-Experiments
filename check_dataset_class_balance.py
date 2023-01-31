7d6bc7bed7354881788833226ee904dd9c824dc3
nadineelnaggar <Nadine_el_naggar@hotmail.com>
2023-01-31 15:34:29 +0000

    add code to count the number of 0 labels in the suzgun style datasets

---
+++ check_dataset_class_balance.py
@@ -0,0 +1,21 @@
+import Dyck1_Datasets
+from Dyck1_Datasets import NextTokenPredictionTrainDataset, NextTokenPredictionValidationDataset, NextTokenPredictionShortTestDataset, NextTokenPredictionLongTestDataset
+
+
+
+
+
+train_0s = 0
+train_1s = 0
+
+val_0s = 0
+val_1s = 0
+
+long_0s = 0
+long_1s = 0
+
+
+train_dataset = NextTokenPredictionTrainDataset()
+val_dataset = NextTokenPredictionValidationDataset()
+long_dataset = NextTokenPredictionLongTestDataset()
+
