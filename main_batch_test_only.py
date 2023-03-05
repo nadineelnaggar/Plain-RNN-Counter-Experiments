e21d2949df7e77deca1c5ed03d81c2cbef761cb5
nadineelnaggar <Nadine_el_naggar@hotmail.com>
2023-03-05 21:40:43 +0000

    uncomment lines 362-365

--- main_batch_test_only.py
+++ main_batch_test_only.py
@@ -266,7 +266,7 @@ optimname = path+ 'Dyck1_' + task + '_' + str(
 test_log = path+ 'Dyck1_' + task + '_' + str(
         num_bracket_pairs) + '_bracket_pairs_' + model_name + '_Feedback_' + feedback + '_' +str(batch_size) +'_batch_size_'+'_' + str(
         hidden_size) + 'hidden_units_' + use_optimiser + '_lr=' + str(learning_rate) + '_' + str(
-        num_epochs) + 'epochs_'+str(lr_scheduler_step)+"lr_scheduler_step_"+str(lr_scheduler_gamma)+"lr_scheduler_gamma_"+ str(num_runs)+'runs_'+str(checkpoint_step)+"checkpoint_step_"+str(num_checkpoints)+"checkpoints" + '_TEST_LOG_INFERENCE.txt'
+        num_epochs) + 'epochs_'+str(lr_scheduler_step)+'lr_scheduler_step_'+str(lr_scheduler_gamma)+'lr_scheduler_gamma_'+ str(num_runs)+'runs_'+str(checkpoint_step)+'checkpoint_step_'+str(num_checkpoints)+'checkpoints' + '_TEST_LOG_INFERENCE.txt'
 long_test_log = path+ 'Dyck1_' + task + '_' + str(
         num_bracket_pairs) + '_bracket_pairs_' + model_name + '_Feedback_' + feedback + '_' +str(batch_size) +'_batch_size_'+'_' + str(
         hidden_size) + 'hidden_units_' + use_optimiser + '_lr=' + str(learning_rate) + '_' + str(
